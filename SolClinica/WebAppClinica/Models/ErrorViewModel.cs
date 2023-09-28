22075a0c87a2ba06252f3bfd038e1c161beee7d5
Rodrigo Carbonel <rcarbonel4471@gmail.com>
2023-09-28 13:34:02 -0500

    este es la estructura del proyecto

---
+++ SolClinica/WebAppClinica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppClinica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
